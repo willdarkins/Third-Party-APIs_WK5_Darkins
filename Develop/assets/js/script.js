15d25e191687ba4d232d3cb6d2e5671a971d7e6d
Will Darkins <willdarkins@gmail.com>
2021-09-05 13:24:43 -0700

    structure set but tasks difficult to code

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,1 +1,6 @@
 var todayDate = $('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
+var tasks = {};
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };

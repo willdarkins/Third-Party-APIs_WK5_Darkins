9befdcbbd5d89f15f0e1b944a682ddadce164b61
Will Darkins <willdarkins@gmail.com>
2021-09-05 14:56:31 -0700

    local storage issues

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,22 @@
 var todayDate = $('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
 var tasks = {};
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+var loadTasks = function () {
+  JSON.parse(localStorage.getItem('tasks')) || [];
+}
 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+$('#save-btn').click(function() {
+  var taskText = $("#task-description").val();
+  var taskTime = $("#hour").val();
+
+  var completeTask = {
+    text: taskText,
+    date: taskTime,
   };
+  tasks.push(completeTask);
+  saveTasks();
+})
+
+loadTasks();

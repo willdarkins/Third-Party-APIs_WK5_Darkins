8f0e5518b19bc6a30cb378e9af943f6f74181029
Will Darkins <willdarkins@gmail.com>
2021-09-10 21:09:15 -0700

    display local storage issues

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,14 +1,14 @@
 var todayDate = $('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
-var tasks = {};
+var tasks = [];
 var time = ['9AM','10AM','11AM','12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 var loadTasks = function() {
-  tasks = tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 }
 var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-function showText(hour) {
+function showText(hour, input) {
   var row = $('<div>');
   var timeContainer = $('<div>');
   var textBlock = $('<textarea>');
@@ -18,7 +18,7 @@ function showText(hour) {
 
   row.addClass("row g-0");
   timeContainer.addClass('col-md-1 hour').text(hour);
-  textBlock.addClass('col-md-10 text-task').text();
+  textBlock.addClass('col-md-10 text-task').val();
   divButton.addClass('col-md-1');
   button.addClass('btn btn-primary saveBtn').attr('style', "padding: 38.5px 45px", 'border-radius: 0 15px 15px 0')
   svgGraphic.addClass('bi-save').attr('style', 'color:white')
@@ -33,12 +33,13 @@ function showText(hour) {
 $('.container').append(row);      
 }
 
-time.forEach(hour => showText(hour,tasks))
+time.forEach(hour => showText(hour))
+tasks.forEach(input => showText(input))
 
 $('.saveBtn').click(function(){
 
-    var taskText = $('.text-task').val();
-    var taskTime = $('.hour').text();
+    var taskText = $(this).parent().siblings('.text-task').val();
+    var taskTime = $(this).parent().siblings('.hour').text();
 
     var completeTask = {
       text: taskText,
@@ -47,5 +48,6 @@ $('.saveBtn').click(function(){
     tasks.push(completeTask);
     saveTasks();
 })
+
 showText();
 loadTasks();

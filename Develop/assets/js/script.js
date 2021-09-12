fa93a24b548ee612ce25175596584eeac1e04d90
Will Darkins <willdarkins@gmail.com>
2021-09-12 12:13:57 -0700

    small changes

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,14 +1,16 @@
+//Current date displayed at top of page
 var todayDisplayDate = $('#currentDay').text(moment().format("dddd, MMMM Do YYYY"));
 
-var tasks = [];
-var time = ['9AM','10AM','11AM','12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-var hours =[9, 10, 11, 12, 13, 14, 15, 16, 17]
+//Global variables for displayed time and corresponding military hours
+var time = ['9', '10', '11', '12', '13', '14', '15', '16', '17'];
+var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
+//Save & load local storage
 var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-var loadTasks = function() {
+var loadTasks = function () {
   tasks = JSON.parse(localStorage.getItem('tasks')) || [];
   tasks.forEach(task => {
     var hourChange = $('#' + task.time)
@@ -16,63 +18,63 @@ var loadTasks = function() {
   })
 }
 
+//Iterate through the time array in order to display time to user
 time.forEach(hour => showText(hour))
 
+//Dynamically creating elements to append to html
 function showText(hour) {
   var row = $('<div>');
   var timeContainer = $('<div>');
   var textBlock = $('<textarea>');
   var divButton = $('<div>');
   var button = $('<button>');
-  var svgGraphic= $('<svg>');
+  var svgGraphic = $('<svg>');
 
   row.addClass("row g-0").attr('id', hour + 'color');
   timeContainer.addClass('col-md-1 hour').text(hour);
-  textBlock.addClass('col-md-10 text-task').attr('id',hour);
+  textBlock.addClass('col-md-10 text-task').attr('id', hour);
   divButton.addClass('col-md-1');
   button.addClass('btn btn-primary saveBtn').attr('style', "padding: 38.5px 45px", 'border-radius: 0 15px 15px 0')
   svgGraphic.addClass('bi-save').attr('style', 'color:white')
   
+
   row.append(timeContainer);
   row.append(textBlock);
   divButton.append(button);
   button.append(svgGraphic);
   row.append(divButton);
 
-
-$('.container').append(row);
+  $('.container').append(row);
 }
 
+//For loop that will set conditional statements to check each row's relation to time (i.e. past, present, future)
+for (var i = 0; i < hours.length; i++) {
+  var currentHour = parseInt(moment().format('H'));
 
+  if (hours[i] < currentHour) {
+    $('#' + time[i] + 'color').addClass('past')
+  }
+  else if (hours[i] === currentHour) {
+    $('#' + time[i] + 'color').addClass('present')
+  }
+  else {
+    $('#' + time[i] + 'color').addClass('future')
+  };
+}
 
-$('.saveBtn').click(function(){
+//Click event on save button that will grab user input and the corresponding hour to store in localstorage
+$('.saveBtn').click(function () {
 
-    var taskText = $(this).parent().siblings('.text-task').val();
-    var taskTime = $(this).parent().siblings('.hour').text();
+  var taskText = $(this).parent().siblings('.text-task').val();
+  var taskTime = $(this).parent().siblings('.hour').text();
 
-    var completeTask = {
-      text: taskText,
-      time: taskTime,
-    }
-    tasks.push(completeTask);
-    saveTasks();
+  var completeTask = {
+    text: taskText,
+    time: taskTime,
+  }
+  tasks.push(completeTask);
+  saveTasks();
 })
+
 loadTasks();
 showText();
-
-
- 
-var currentHour = parseInt(moment().format('H'));
-
-console.log(currentHour);
-
-if(hours[0] < currentHour) {
-  $('#3PMcolor').addClass('past')
-}
-else if(hours[0] === currentHour) {
-  $('#3PMcolor').addClass('present')
-}
-else { 
-  $('#3PMcolor').addClass('future')
-};
-
